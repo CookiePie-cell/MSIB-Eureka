b5f7cd833bac14b81b21362e2dad9bf987bdf773
Julio Nicholas <juliojnicolas1@gmail.com>
2023-08-29 17:04:36 +0700

    add crashlytics and fix bugs

---
+++ ToDoList/app/src/main/java/com/salugan/todolist/ui/activity/splash/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.salugan.todolist.ui.activity.splash
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.salugan.todolist.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
