d1155bfe1cd87c03bc3ac1f7903e2a5976ffff21
Joseph Julio Nicholas <70589063+CookiePie-cell@users.noreply.github.com>
2023-08-31 14:32:36 +0700

    Update ApiConfig.kt

--- GithubUser/app/src/main/java/com/salugan/githubuser/data/remote/retrofit/ApiConfig.kt
+++ GithubUser/app/src/main/java/com/salugan/githubuser/data/remote/retrofit/ApiConfig.kt
@@ -12,7 +12,7 @@ class ApiConfig {
     companion object {
         private const val BASE_URL = "https://api.github.com/"
 
-        private const val TOKEN = "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"
+        private const val TOKEN = "MASUKKAN_TOKEN"
 
         private fun provideOkHttpClient(): OkHttpClient {
 
